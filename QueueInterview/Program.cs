e773f62e9db725e66b7cb639c14a7a089911c20e
abbastoz <abbastoz@hotmail.com>
2022-09-14 08:01:24 +0300

    rename namespace

--- QueueInterview/Program.cs
+++ QueueInterview/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
-namespace QueueInterview
+namespace CsharpInterviewQuestion
 {
     class Program
     {
--- QueueInterview/Student.cs
+++ QueueInterview/Student.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace QueueInterview
+namespace CsharpInterviewQuestion
 {
    public class Student:Person
     {
